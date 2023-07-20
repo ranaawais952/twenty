bbffbb82d4e64616f890b80afa8ff56545a3183e
Rana Owais Rasool <awaisrasool777@gmail.com>
2023-07-20 09:54:51 +0500

    Any Text!!!

---
+++ file2.js
@@ -0,0 +1,14 @@
+let x = myFunction(4, 3);
+console.log("X is working...!");
+
+function myFunction(a, b) {
+    return a * b;
+  }
+
+function myFunction(a, b) {
+  return a * b;
+}
+
+function myFunction(a, b) {
+    return a * b;
+  }
